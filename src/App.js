631c9587068428d3813f2e9c77b5b00bed5ebd49
lfdagm <lfdagm@gmail.com>
2023-10-29 00:23:43 -0700

    Adding Bootstrap CDN and creating a test NavBar component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Vancomer
-        </p>
+        <NavBar />
       </header>
     </div>
   );
