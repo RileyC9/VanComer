5bdd641a2260f7df95c509913c0d04da370c798c
lfdagm <lfdagm@gmail.com>
2023-11-06 14:47:29 -0800

    Adding login button and removing NavDropDown for now

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,26 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import Button from 'react-bootstrap/Button';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function NavBar() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar fixed="top" expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">VanComer</Navbar.Brand>
+        <Navbar.Brand href="#home"><img
+              src={require("./../img/logo_transparent.png")}
+              width="100%"
+              height="60"
+              className="d-inline-block align-top"
+              alt="VanComer Logo"
+            /></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#documentation">Documentation</Nav.Link>
+            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Another action
@@ -23,8 +30,9 @@ function NavBar() {
               <NavDropdown.Item href="#action/3.4">
                 Separated link
               </NavDropdown.Item>
-            </NavDropdown>
+            </NavDropdown> */}
           </Nav>
+          <Button variant="outline-primary">Login</Button>{' '}
         </Navbar.Collapse>
       </Container>
     </Navbar>
