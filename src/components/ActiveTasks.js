7d7842dba0bcc3527cf5c2639d1b7c4cb8e531a9
Xav92 <xavier.pardanaud@gmail.com>
2023-11-19 17:40:27 -0800

    Active tasks of client

--- src/components/ActiveTasks.js
+++ src/components/ActiveTasks.js
@@ -1,11 +1,27 @@
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Form from 'react-bootstrap/Form';
+
 
 function ActiveTask(props){
    
-    return <div className="card">
-        <h2>{props.text}</h2>
-        <li>hello</li>
-
-    </div>
-
-}
+    return (
+<>
+      <Card border="primary" style={{ width: '30rem' }}>
+        <Card.Header>Active Tasks</Card.Header>
+        <Card.Body>
+          <ListGroup className="list-group-flush">
+              {props.tasks.map((task,index)=>(
+                <ListGroup.Item key={index}>
+                    {task.taskName}
+                    <Form.Check aria-label="notActive" onChange={()=> props.onCheckboxChange(task)}/>
+                </ListGroup.Item>
+                
+             ))}
+              </ListGroup>
+        </Card.Body>
+      </Card>
+      <br />
+    </>
+)}
 export default ActiveTask;
