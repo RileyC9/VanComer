07f11e94127d7281b4620033f2bba3b04fe73578
Xav92 <xavier.pardanaud@gmail.com>
2023-11-19 17:41:35 -0800

    the historic tasks of the client

--- src/components/Historic.js
+++ src/components/Historic.js
@@ -1,7 +1,20 @@
-function Historic(props){
-    return <div className="card">
-        <h2>{props.text}</h2>
-    </div>
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
 
-}
+function Historic(props){
+    return (
+        <>
+              <Card border="danger" style={{ width: '30rem' }}>
+                <Card.Header>Tasks History</Card.Header>
+                <Card.Body>
+                  <ListGroup className="list-group-flush">
+                    {props.selectedTasks.map((task,index)=>(
+                        <ListGroup.Item key={index}>{task.taskName}</ListGroup.Item>
+                    ))}
+                      </ListGroup>
+                </Card.Body>
+              </Card>
+              <br />
+            </>
+        )}
 export default Historic;
