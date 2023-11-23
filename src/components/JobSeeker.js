a0966d7181d709d8f8bf3e7c3fbc03b54cda73c4
Xav92 <xavier.pardanaud@gmail.com>
2023-11-23 13:05:15 -0800

    version correction

---
+++ src/components/JobSeeker.js
@@ -0,0 +1,16 @@
+import ActiveTask from "./ActiveTasks";
+import PotentialJobs from "./pages/PotentialJobs";
+
+
+function jobSeeker (props){
+return(
+    <>
+    {/* <ActiveTask/> */}
+    {/* <PotentialJobs/> */}
+    </>
+
+    
+)
+}
+
+export default jobSeeker;
