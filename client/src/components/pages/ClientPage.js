63a5e2834424fad5894de35504169d1add1aba10
lfdagm <lfdagm@gmail.com>
2023-11-27 01:31:51 -0800

    Implemented Grid for the container to center content

--- client/src/components/pages/ClientPage.js
+++ client/src/components/pages/ClientPage.js
@@ -1,54 +1,73 @@
-import axios from 'axios';
-import { useState, useEffect} from 'react';
-import ActiveTask from '../ActiveTasks';
-import Historic from '../Historic';
-import FormModal from '../Modal';
+import axios from "axios";
+import { useState, useEffect } from "react";
+import ActiveTask from "../ActiveTasks";
+import Historic from "../Historic";
+import FormModal from "../Modal";
+import { div } from "prelude-ls";
 
-export default function ClientPage () {
-  const [activeTasks,setActiveTasks] = useState([]);
-  const[selectedTasks,setSelectedTasks] = useState([]);
-  const [ historicTasks,setHistoricTasks] = useState([]);
+import { MDBContainer, MDBRow, MDBCol } from "mdb-react-ui-kit";
+
+export default function ClientPage() {
+  const [activeTasks, setActiveTasks] = useState([]);
+  const [selectedTasks, setSelectedTasks] = useState([]);
+  const [historicTasks, setHistoricTasks] = useState([]);
   const [check, setCheck] = useState(false);
-// fetch the data from server and store all the jobpost related to this user id in jobPosts, and localStorage (key: 'jobPosts')
+  // fetch the data from server and store all the jobpost related to this user id in jobPosts, and localStorage (key: 'jobPosts')
   useEffect(() => {
-    const user = JSON.parse(localStorage.getItem('user'));
+    const user = JSON.parse(localStorage.getItem("user"));
     const userId = user.userId;
-    axios.get('http://localhost:3500/api/jobs/' + userId).then((repos) => {
+    axios.get("http://localhost:3500/api/jobs/" + userId).then((repos) => {
       let jobPosts = repos.data;
-      localStorage.setItem('jobPosts', JSON.stringify(jobPosts));
+      localStorage.setItem("jobPosts", JSON.stringify(jobPosts));
       setCheck(true);
     });
-  },[]);
+  }, []);
 
-  const handleFormSubmit = (formData)=>{
+  const handleFormSubmit = (formData) => {
     // setActiveTasks([...activeTasks,formData])
-    setActiveTasks([...activeTasks,{taskName:formData.taskname}])
-  }
+    setActiveTasks([...activeTasks, { taskName: formData.taskname }]);
+  };
 
-  const handleCheckboxChange = (task)=>{
-    if(selectedTasks.some((t)=> t.taskName === task.taskName)){
-      
-      setSelectedTasks(selectedTasks.filter((t)=> t.taskName !== task.taskName));
-      setSelectedTasks([...selectedTasks,task]);
+  const handleCheckboxChange = (task) => {
+    if (selectedTasks.some((t) => t.taskName === task.taskName)) {
+      setSelectedTasks(
+        selectedTasks.filter((t) => t.taskName !== task.taskName)
+      );
+      setSelectedTasks([...selectedTasks, task]);
+    } else {
+      setHistoricTasks([...historicTasks, task]);
     }
-    else{
-      setHistoricTasks([...historicTasks,task])
-    }
-  }
-  return (
-    (check?<div className="card">
-      <h2 className="mx-auto">Client Page</h2>
-      <div className="actions">
-        <div className="w-50 p-3 d-inlineBlock">
-          <ActiveTask onCheckboxChange={handleCheckboxChange}/>
-        </div>
-        <div className="w-50 p-3 d-inlineBlock">
+  };
+  return check ? (
+    <MDBContainer fluid>
+      <MDBRow className="text-center my-3">
+        <MDBCol>
+          <h2>Client Page</h2>
+        </MDBCol>
+      </MDBRow>
+      <MDBRow className="mb-4">
+        <MDBCol></MDBCol>
+        <MDBCol>
+          <ActiveTask onCheckboxChange={handleCheckboxChange} />
+        </MDBCol>
+        <MDBCol></MDBCol>
+      </MDBRow>
+      <MDBRow>
+        <MDBCol></MDBCol>
+        <MDBCol>
           <Historic />
-        </div>
-        <div>
-        <FormModal onFormSubmit={handleFormSubmit}/> 
-        </div>
-      </div>
-    </div>: <></>)
+        </MDBCol>
+        <MDBCol></MDBCol>
+      </MDBRow>
+      <MDBRow>
+        <MDBCol></MDBCol>
+        <MDBCol>
+          <FormModal onFormSubmit={handleFormSubmit} />
+        </MDBCol>
+        <MDBCol></MDBCol>
+      </MDBRow>
+    </MDBContainer>
+  ) : (
+    <></>
   );
 }
